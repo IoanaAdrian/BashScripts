75e52143c73055fdfc9136c4f77d078470a4af81
IoanaAdrian <adrian212005@yahoo.ro>
2021-07-18 23:04:31 +0300

    initial commit: scbt

---
+++ scbt.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#ScreenBrightnessTemperature
+
+
+function help(){
+        RED='\e[41m'
+        echo -e "${RED}###### THIS IS HELP MODE ######"
+        echo "Use -d to activate the day mode"
+        echo "Use -n to activate the night mode"
+        echo -e "\e[40m"
+}
+dateInterval=""
+config=$(cat ./scbt.config)
+for word in $config; do
+    isInterval=false
+    for (( i=0; i<${#word}; i++ )); do
+        if [ ${word:$i:1} = "-" ]
+        then
+            isInterval=true
+        fi
+    done
+    if [ $isInterval = true ]
+    then
+        dateInterval=$word
+    fi
+done
+
+if [ $# -gt 0 ]
+then 
+    if [ $1 = -h ]
+    then
+        help
+    fi
+    if [ $1 = "-d" ]
+    then
+        sct 6500
+        xrandr --output HDMI-0 --brightness 1
+        xrandr --output HDMI-1 --brightness 1
+    else if [ $1 = "-n" ]
+        then 
+            sct 4500
+            xrandr --output HDMI-0 --brightness 0.8
+            xrandr --output HDMI-1 --brightness 0.8
+        fi
+    fi
+fi
